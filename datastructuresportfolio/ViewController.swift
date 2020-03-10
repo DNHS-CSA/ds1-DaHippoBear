6f87839622c4a22ce3b197c6958c2deb3c41d188
dd <ddddddd>
2020-03-10 10:10:11 -0700

    s

--- datastructuresportfolio/ViewController.swift
+++ datastructuresportfolio/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let side1 = 0.0
+    let side2 = 0.0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-         print("hello")
-        let urmum = "reeee"
-        print(urmum)
+       }
+    func pythag() {
+        let side3 = sqrt((side1*side1)+(side2*side2))
+        print(side3)
         
+     
     }
    
 
